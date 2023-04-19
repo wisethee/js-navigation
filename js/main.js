b5c500cc6000e60469cdafacd98e60e3956506b2
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-04-19 20:54:52 +0100

    feat: add animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-document.addEventListener('DOMContentLoaded', () =>{
-  console.log('DOM Content Loaded and Parsed!');
+document.addEventListener("DOMContentLoaded", () => {
+  const navigationList = document.querySelector(".navigation-list");
+  const navigationItems = document.querySelectorAll(".navigation-item");
+  const navigationActiveItem = document.querySelector(
+    ".navigation-item.active"
+  );
+
+  const navigationSlide = document.querySelector(".navigation-slide");
+
+  const navigationWidth = navigationActiveItem.offsetWidth;
+  const navigationLeft = navigationActiveItem.offsetLeft;
+
+  navigationItems.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      navigationSlide.style.width = `${item.offsetWidth}px`;
+      navigationSlide.style.left = `${item.offsetLeft}px`;
+      navigationSlide.style.opacity = 1;
+    });
+
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      navigationItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+      navigationSlide.style.width = `${item.offsetWidth}px`;
+      navigationSlide.style.left = `${item.offsetLeft}px`;
+    });
+  });
 });
